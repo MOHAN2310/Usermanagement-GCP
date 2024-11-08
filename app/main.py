d3f6697a8a4e05420428ed157c66d68bef5127be
Mohan Raj <mraj16457@gmail.com>
2024-11-09 01:37:01 +0530

    UM-2 Bugfix: Make send invitation dynamic 1. Handle send invitation to send mail based on the request as dynamically.

--- app/main.py
+++ app/main.py
@@ -34,6 +34,10 @@ class User(BaseModel):
     email: str
     project_id: str
 
+class Inviation(BaseModel):
+    email: str
+    project_id: str
+
 @app.post("/add_users")
 async def add_user(user: User):
     user_id = str(uuid.uuid4())
@@ -63,55 +67,57 @@ async def delete_user(user_id: str):
     user_ref.delete()
     return {"status": "User deleted"}
 
-body = """
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body style="font-family: Arial, sans-serif; margin: 0; padding: 0;">
-  <div style="width: 100%; padding: 20px; background-color: #f4f4f4;">
-    <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">
-      <div style="background-color: #3b82f6; color: #ffffff; padding: 20px; text-align: center; font-size: 24px; font-weight: bold; border-radius: 10px 10px 0 0;">
-        API Documentation Invitation
-      </div>
-      <p>Hello,</p>
-      <p>I am excited to invite you all to view our User Management API documentation on ReDoc.</p>
-      <p>You can access the API documentation via the following links:</p>
-      <ul>
-          <li>
-              <a href="https://check-app-508284549488.us-central1.run.app/docs" style="display: inline-block; background-color: #3b82f6; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 5px; font-size: 16px;">View Swagger Documentation</a>
-          </li>
-          <li>
-              <a href="https://check-app-508284549488.us-central1.run.app/redoc" style="display: inline-block; background-color: #3b82f6; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 5px; font-size: 16px;">View ReDoc Documentation</a>
-          </li>
-      </ul>
-      <p>For more information, check out our GitHub repository:</p>
-      <p>
-          <a href="" style="display: inline-block; background-color: #28a745; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 5px; font-size: 16px;">Visit GitHub Repository</a>
-      </p>
-      <p>Look forward to your feedback.</p>
-      <div style="margin-top: 20px; font-size: 14px; color: #666666; text-align: center;">
-        Thank you,<br>
-        Mohan Raj B<br>
-        <i>If you have any questions, feel free to reply to this email.</i>
-      </div>
-    </div>
-  </div>
-</body>
-</html>
-"""
 
-
-
-@app.get("/send_invite")
-async def send_invite():
-    yag = yagmail.SMTP(SENDER_MAIL, SENDER_MAIL_PASS)
-    receiver_email = "mohan16457@gmail.com"
-    subject = "API Documentation Invitation"
-    html_content = body
-    yag.send(to=receiver_email, subject=subject, contents=html_content)
-    print("Email sent successfully!")
+@app.post("/send_invite")
+async def send_invite(invitation: Inviation):
+    html_content = f"""
+    <!DOCTYPE html>
+    <html>
+    <head></head>
+    <body style="font-family: Arial, sans-serif; margin: 0; padding: 0;">
+        <div style="width: 100%; padding: 20px; background-color: #f4f4f4;">
+        <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 20px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);">
+            <div style="background-color: #3b82f6; color: #ffffff; padding: 20px; text-align: center; font-size: 24px; font-weight: bold; border-radius: 10px 10px 0 0;">
+            API Documentation Invitation
+            </div>
+            <p>Hello,</p>
+            <p>I am excited to invite you all to view our User Management API documentation on ReDoc for the project ID: <strong>{invitation.project_id}</strong>.</p>
+            <p>You can access the API documentation via the following links:</p>
+            <ul>
+                <li>
+                    <a href="https://check-app-508284549488.us-central1.run.app/docs" style="display: inline-block; background-color: #3b82f6; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 5px; font-size: 16px;">View Swagger Documentation</a>
+                </li>
+                <li>
+                    <a href="https://check-app-508284549488.us-central1.run.app/redoc" style="display: inline-block; background-color: #3b82f6; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 5px; font-size: 16px;">View ReDoc Documentation</a>
+                </li>
+            </ul>
+            <p>For more information, check out our GitHub repository:</p>
+            <p>
+                <a href="https://github.com/MOHAN2310/Usermanagement-GCP.git" style="display: inline-block; background-color: #28a745; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 5px; font-size: 16px;">Visit GitHub Repository</a>
+            </p>
+            <p>Look forward to your feedback.</p>
+            <div style="margin-top: 20px; font-size: 14px; color: #666666; text-align: center;">
+            Thank you,<br>
+            Mohan Raj B<br>
+            <i>If you have any questions, feel free to reply to this email.</i>
+            </div>
+        </div>
+        </div>
+    </body>
+    </html>
+    """
+    try:
+        yag = yagmail.SMTP(SENDER_MAIL, SENDER_MAIL_PASS)
+        receiver_email = [invitation.email]
+        subject = "API Documentation Invitation"
+        html_content = html_content
+        yag.send(to=receiver_email, subject=subject, contents=html_content)
+        print("Email sent successfully!")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Failed to send email. Please try again later.")
     return {"status": "Invitation mail sent successfully!"}
+
+
 
 
 if __name__ == "__main__":
